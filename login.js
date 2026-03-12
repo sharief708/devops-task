a19c23767971828b0b279b9ee693ed182c009f51
Umar Sharief <umarsharief708@gmail.com>
2026-03-12 11:54:27 +0530

    Added Login API

---
+++ login.js
@@ -0,0 +1,9 @@
+function login(username, password) {
+  if (username === "admin" && password === "1234") {
+    return "Login successful";
+  } else {
+    return "Invalid credentials";
+  }
+}
+
+module.exports = login;
